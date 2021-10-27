09f37bf5a39b24ce37622541651a37dd603ba8bb
DZCM4G4 <ext.daniel.vacha@skoda-auto.cz>
2021-10-27 23:12:14 +0200

    add xml service

--- src/main/java/com/example/models/Book.java
+++ src/main/java/com/example/models/Book.java
@@ -5,7 +5,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "Book")
+@XmlRootElement(name = "book")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Book {
     @XmlAttribute
